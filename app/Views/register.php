34cca0fe3edb9484ce9d239f9c0338f74241c5b5
mnaufal-helmi@gmail.com <mnaufalhelmi@gmail.com>
2022-05-17 18:47:39 +0700

    third

--- app/Views/register.php
+++ app/Views/register.php
@@ -1,11 +1,11 @@
 <?= $this->extend('layout') ?>
 <?= $this->section('content')?>
-<?php 
+<?php
 $username = [
     'name' => 'username',
     'id' => 'username',
     'value' => null,
-    'class' => 'form-control' 
+    'class' => 'form-control'
 ];
 $password = [
     'name' => 'password',
@@ -19,23 +19,23 @@ $repeatPassword = [
 ];
 $session = session();
 $errors = $session->getFlashdata('errors');
-    ?>
+?>
 <h1> register form</h1>
-<?php if($errors != null): ?>
+<?php if ($errors != null) : ?>
     <div class="alert alert-danger" role="alert">
         <h4 class="alert-heading">Terjadi kesalahan</h4>
-    <hr>
-    <p class="mb-0">
-    <?php
-                foreach($errors as $err){
-                    echo $err.'<br>';
-                }
-        ?>
-       
+        <hr>
+        <p class="mb-0">
+            <?php
+            foreach ($errors as $err) {
+                echo $err . '<br>';
+            }
+            ?>
+
         </p>
     </div>
-    <?php endif ?>
-<?= form_open('Auth/register')?>
+<?php endif ?>
+<?= form_open('Auth/register') ?>
 <div class="form-group">
     <?= form_label("Username", "username") ?>
     <?= form_input($username) ?>
@@ -51,7 +51,7 @@ $errors = $session->getFlashdata('errors');
 </div>
 
 <div class="text-right">
-    <?= form_submit ('submit','Submit',['class'=>'btn btn-primary']) ?>
+    <?= form_submit('submit', 'Submit', ['class' => 'btn btn-primary']) ?>
 </div>
-<?= form_close()?>
-<?= $this->endSection()?>
+<?= form_close() ?>
+<?= $this->endSection() ?>
